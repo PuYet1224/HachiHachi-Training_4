c6d200d14b843e8e333e6f0af9446ee32d465f4c
PuYet1224 <lala0001112223336@gmail.com>
2025-01-16 20:02:29 +0700

    Day 16

--- src/app/pipes/overdue-days-color.pipe.ts
+++ src/app/pipes/overdue-days-color.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OverdueDaysColorPipe implements PipeTransform {
   transform(days: number): string {
     if (days === 3) {
-      return '#FFCDD2';
+      return '#E47C56';
     } else if (days === 2) {
       return '#FF5722';
     } else if (days === 1) {
-      return '#D32F2F';
+      return '#ED1717';
     }
     return '';
   }
